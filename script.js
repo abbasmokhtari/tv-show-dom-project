31011b22b21f07064b4168f8e28af16d095f1c98
Abbas Mokhtari <abs.mokhtari@gmail.com>
2020-05-23 23:13:35 +0100

    title and episode and season made

--- script.js
+++ script.js
@@ -5,8 +5,41 @@ function setup() {
 }
 
 function makePageForEpisodes(episodeList) {
-  const rootElem = document.getElementById("root");
-  rootElem.textContent = `Got ${episodeList.length} episode(s)`;
+  const rootElem = document.getElementById('root');
+  episodeList
+    .map(x => makeDiv(x.name, x.season, x.number))
+    .forEach(x => rootElem.appendChild(x))
+  // rootElem.textContent = `Got ${episodeList.length} episode(s)`;
 }
+
+let zeroAdd = (num) => {
+  if (num > 9) {
+    return num
+  } else {
+    return '0' + num
+  }
+}
+
+// getting the name of an episode
+
+let makeTitle = (title, season, number) => {
+  let titleElement = document.createElement('h3');
+  titleElement.innerText = `${title} - S${zeroAdd(season)}E${zeroAdd(number)}`;
+  return titleElement
+}
+
+
+
+// making the main DIV
+
+let makeDiv = (title, season, number) => {
+  let newDiv = document.createElement('div');
+  let newTitle = makeTitle(title, season, number);
+  newDiv.appendChild(newTitle);
+  return newDiv
+}
+
+
+
 
 window.onload = setup;
